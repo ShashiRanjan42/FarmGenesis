57265d6bdf1c600a7aacb87237164d39d0ae492f
ShashiRanjan42 <ranjanshashi323@gmail.com>
2024-07-20 13:04:17 +0530

    Commit

--- frontend/src/Services/apis.js
+++ frontend/src/Services/apis.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://farmgenesis-backend.onrender.com/api";
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://farmgenesis-backend.onrender.com/api";
+// const BASE_URL = "http://localhost:8000/api";
 const BASE_URL_ML = "http://localhost:5000";
 
 export const endpoints = {
